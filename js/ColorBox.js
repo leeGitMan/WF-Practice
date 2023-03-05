a62b311db214b36fb9be00f2bef76494fd8a5dae
leeGitMan <gosrod05@gmail.com>
2023-03-05 22:51:48 +0900

    commit

--- js/ColorBox.js
+++ js/ColorBox.js
@@ -24,4 +24,37 @@ for(let i = 0; i < inputArr.length; i++){
 }
 
 
+
+// selectQuery로 해보기
+
+// 인풋 요소, 박스 요소들을 먼저 가져온다. 배열 형태로 변수에 담아주기
+
+const inputArr2 = document.querySelectorAll(".wrap2 > input")
+const boxArr2 = document.querySelectorAll(".wrap > div");
+
+for(let i = 0; i < inputArr2.length; i++){
+    inputArr2[i].addEventListener("keyup", () => {
+        boxArr2[i].style.backgroundColor = inputArr2[i].value;
+    })
+}
+
+
+// 버튼으로 트랜지션 입혀서 색상 바꿔보기
+
+const btn = document.getElementsByClassName("btn1");
+const boxArr3 = document.getElementsByClassName("div4");
+const inputArr3 = document.querySelectorAll(".wrap3 > input");
+
+for(let i = 0; i < btn.length; i++){
+    btn[i].addEventListener("click", () => {
+        boxArr3[i].style.backgroundColor = inputArr3[i].value;
+        boxArr3[i].classList.add("transition");
+    })
+}
+
+
+
+
+
+
     
